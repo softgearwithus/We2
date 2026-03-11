17219d0eb2a2095f928ed6fbacdbb10373e9312f
ayush <technologiessoftgear@gmail.com>
2026-03-11 10:46:20 +0530

    fix(test): stub queries deps in specs

--- backend/src/queries/queries.controller.spec.ts
+++ backend/src/queries/queries.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QueriesController } from './queries.controller';
+import { QueriesService } from './queries.service';
 
 describe('QueriesController', () => {
   let controller: QueriesController;
@@ -7,6 +8,12 @@ describe('QueriesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [QueriesController],
+      providers: [
+        {
+          provide: QueriesService,
+          useValue: {}
+        }
+      ],
     }).compile();
 
     controller = module.get<QueriesController>(QueriesController);
--- backend/src/queries/queries.service.spec.ts
+++ backend/src/queries/queries.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { QueriesService } from './queries.service';
+import { Query } from './entities/query.entity';
 
 describe('QueriesService', () => {
   let service: QueriesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [QueriesService],
+      providers: [
+        QueriesService,
+        {
+          provide: getRepositoryToken(Query),
+          useValue: {}
+        }
+      ],
     }).compile();
 
     service = module.get<QueriesService>(QueriesService);
